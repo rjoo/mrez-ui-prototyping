d29b075b9fc7be15eb907747c004617cf4f95048
williampansky <williampansky@gmail.com>
2018-09-04 10:51:01 -0500

    Added vue-styleguideist

---
+++ src/components/AppButton.vue
@@ -0,0 +1,83 @@
+<template>
+  <button 
+    :style="styles" 
+    @click="handleClick">
+    <!-- @slot Use this slot to place the button content -->
+    <slot />
+  </button>
+</template>
+
+<script>
+    import sizeMixin from './sizeMixin';
+    export default {
+        name: 'AppButton',
+        mixins: [sizeMixin],
+        props: {
+            /**
+             * Sets the button font color
+             */
+            color: {
+                type: String,
+                default: 'black'
+            },
+            /** Sets background color of the button
+             * @since 1.2.0
+             */
+            background: {
+                type: String,
+                default: 'white'
+            }
+        },
+        computed: {
+            styles() {
+                return {
+                    'font-size': this.size,
+                    color: this.color,
+                    background: this.background
+                };
+            }
+        },
+        methods: {
+            /** Triggered when button is clicked
+             * @event click
+             * @type {Event}
+             */
+            /** Event for Alligator's example
+             * @event gator
+             * @type {Event}
+             */
+            handleClick(e) {
+                this.$emit('click', e);
+                this.$emit('gator', e);
+            }
+        }
+    };
+</script>
+
+<docs>
+This button is amazing, use it responsibly.
+
+## Examples
+
+Orange button:
+
+```jsx
+<AppButton color="orange">Push Me</AppButton>
+```
+
+Ugly button with pink font and blue background:
+
+```jsx
+<AppButton color="pink" background="blue">
+  Ugly button
+</AppButton>
+```
+
+Button containing custom tags:
+
+```jsx
+<AppButton>
+  Text with <b>bold</b>
+</AppButton>
+```
+</docs>
